6441463d2c6bffcd5842b4a7ba4dde855b45acbb
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2020-10-12 17:54:12 +0200

    Updates

---
+++ precache-manifest.2e3b4ae64b205eb87c08feee5ee4f8bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6cb4801dcf953ca4928955ff6609cc8",
+    "url": "/todolistreact/index.html"
+  },
+  {
+    "revision": "a9b0eabff227b69e9e98",
+    "url": "/todolistreact/static/css/main.78ae3085.chunk.css"
+  },
+  {
+    "revision": "49b9e561cdab0fc7e188",
+    "url": "/todolistreact/static/js/2.889f1ce7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolistreact/static/js/2.889f1ce7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b0eabff227b69e9e98",
+    "url": "/todolistreact/static/js/main.e60a5889.chunk.js"
+  },
+  {
+    "revision": "095cab64ba743c6e0b1a",
+    "url": "/todolistreact/static/js/runtime-main.05ff4f8f.js"
+  }
+]);
